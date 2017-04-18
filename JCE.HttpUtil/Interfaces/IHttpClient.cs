69fbf02aa82484350bc991d8ef70a6c7e59c5f4f
jian玄冰 <jianxuanhuo1@126.com>
2017-04-19 00:04:46 +0800

    添加HTTP工具库

---
+++ JCE.HttpUtil/Interfaces/IHttpClient.cs
@@ -0,0 +1,38 @@
+﻿/************************************************************************************
+ * Copyright (c) 2017 All Rights Reserved. 
+ * CLR版本：4.0.30319.42000
+ * 机器名称：JIAN
+ * 命名空间：JCE.HttpUtil.Interfaces
+ * 文件名：IHttpClient
+ * 版本号：v1.0.0.0
+ * 唯一标识：964965f7-6285-4580-9bba-d63687c46d41
+ * 当前的用户域：JIAN
+ * 创建人：简玄冰
+ * 电子邮箱：jianxuanhuo1@126.com
+ * 创建时间：2017/4/18 23:57:18
+ * 描述：
+ *
+ * =====================================================================
+ * 修改标记：
+ * 修改时间：2017/4/18 23:57:18
+ * 修改人：简玄冰
+ * 版本号：v1.0.0.0
+ * 描述：
+ *
+/************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCE.HttpUtil.Interfaces
+{
+    /// <summary>
+    /// 用于 HTTP 请求的发送和接收
+    /// </summary>
+    public interface IHttpClient
+    {
+
+    }
+}
---
+++ JCE.HttpUtil/Interfaces/IHttpResponse.cs
@@ -0,0 +1,37 @@
+﻿/************************************************************************************
+ * Copyright (c) 2017 All Rights Reserved. 
+ * CLR版本：4.0.30319.42000
+ * 机器名称：JIAN
+ * 命名空间：JCE.HttpUtil.Interfaces
+ * 文件名：IHttpResponse
+ * 版本号：v1.0.0.0
+ * 唯一标识：05e7b984-9ff9-4017-9107-c86f3f9ac7b4
+ * 当前的用户域：JIAN
+ * 创建人：简玄冰
+ * 电子邮箱：jianxuanhuo1@126.com
+ * 创建时间：2017/4/18 23:57:49
+ * 描述：
+ *
+ * =====================================================================
+ * 修改标记：
+ * 修改时间：2017/4/18 23:57:49
+ * 修改人：简玄冰
+ * 版本号：v1.0.0.0
+ * 描述：
+ *
+/************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCE.HttpUtil.Interfaces
+{
+    /// <summary>
+    /// 表示一个 HTTP 响应
+    /// </summary>
+    public interface IHttpResponse
+    {
+    }
+}
