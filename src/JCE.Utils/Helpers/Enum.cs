09808ba2a85bf04035053059083183e9d8837783
jian玄冰 <jianxuanhuo1@126.com>
2017-08-10 18:35:20 +0800

    加入枚举 完成 反射

---
+++ src/JCE.Utils/Helpers/Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCE.Utils.Helpers
+{
+    /// <summary>
+    /// 枚举 操作
+    /// </summary>
+    public static class Enum
+    {
+    }
+}
