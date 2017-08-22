fe4910c660488a683d592b6b417e18676cbc12dd
jian玄冰 <jianxuanhuo1@126.com>
2017-08-22 15:38:41 +0800

    加入图片工具测试

---
+++ JCE.Utils.Test/Medias/ImageUtilTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JCE.Utils.Medias;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JCE.Utils.Test.Medias
+{
+    [TestClass]
+    public class ImageUtilTest
+    {
+        [TestMethod]
+        public void TestImageWatermark()
+        {
+            string imgUrl = "D:\\001.jpg";
+            string waterUrl = "D:\\鸿荣水印图.png";
+            string result=ImageUtil.ImageWatermark(imgUrl, waterUrl, ImageLocationMode.Center);
+            Console.WriteLine(result);
+        }
+    }
+}
