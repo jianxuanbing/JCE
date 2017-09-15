7f10b5827414da42a3af179aace7a35f42ddd81a
jian玄冰 <jianxuanhuo1@126.com>
2017-09-15 16:24:04 +0800

    加入HTTP请求组件

---
+++ JCE.Utils.Http.Test/HttpRequestParameterTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JCE.Utils.Http.Enums;
+using JCE.Utils.Http.Extensions;
+using JCE.Utils.Http.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JCE.Utils.Http.Test
+{
+    [TestClass]
+    public class HttpRequestParameterTest
+    {
+        [TestMethod]
+        public void TestGet()
+        {
+            HttpRequestParameter request=new HttpRequestParameter();
+            request.Method=HttpMethod.Get;
+            request.AddressUrl = "http://localhost:8094/api/Test/GetDefaultConfig";
+            var result =request.SendReq();
+            string content = result.Result.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(content);
+        }
+    }
+}
---
+++ JCE.Utils.Http/Model/HttpRequestParameter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using HttpMethod = JCE.Utils.Http.Enums.HttpMethod;
+
+namespace JCE.Utils.Http.Model
+{
+    /// <summary>
+    /// Http 请求参数
+    /// </summary>
+    public class HttpRequestParameter
+    {
+
+        /// <summary>
+        /// 请求地址
+        /// </summary>
+        public Uri Uri { get; set; }
+
+        /// <summary>
+        /// 请求地址，如果此值设置，则忽略<see cref="Uri"/>值
+        /// </summary>
+        public string AddressUrl { get; set; }
+
+        /// <summary>
+        /// Http请求方式
+        /// </summary>
+        public HttpMethod Method { get; set; } = HttpMethod.Get;
+
+        /// <summary>
+        /// 请求消息 设置方法，如果当前的设置不能满足需求，可以通过这里再次设置
+        /// </summary>
+        public Action<HttpRequestMessage> RequestSettings { get; set; }
+
+        /// <summary>
+        /// 文件参数列表
+        /// </summary>
+        public List<FileParameter> FileParameters { get; set; }
+
+        /// <summary>
+        /// 是否存在文件
+        /// </summary>
+        public bool HasFile => FileParameters.Any();
+
+        /// <summary>
+        /// Form 表单参数，非文件参数列表
+        /// </summary>
+        public List<FormParameter> FormParameters { get; set; }
+
+        /// <summary>
+        /// 自定义内容实体。
+        /// eg:当上传文件时，无法自定义内容
+        /// </summary>
+        public string CustomBody { get; set; }
+
+        /// <summary>
+        /// 超时时间（毫秒）
+        /// </summary>
+        public int TimeOut { get; set; }
+
+        /// <summary>
+        /// 初始化一个<see cref="HttpRequestParameter"/>类型的实例
+        /// </summary>
+        public HttpRequestParameter()
+        {
+            FormParameters=new List<FormParameter>();
+            FileParameters=new List<FileParameter>();
+        }
+    }
+}
