7dc0de7ab403724262e63c4cf8c723c3fdf46a1f
jian玄冰 <jianxuanhuo1@126.com>
2017-09-18 00:07:59 +0800

    加入日志模块

---
+++ src/JCE.Utils/Logs/Core/LogContent.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JCE.Utils.Logs.Abstractions;
+
+namespace JCE.Utils.Logs.Core
+{
+    /// <summary>
+    /// 日志内容
+    /// </summary>
+    public class LogContent:ILogContent
+    {
+        #region Property(属性)
+        /// <summary>
+        /// 日志名称
+        /// </summary>
+        public string LogName { get; set; }
+
+        /// <summary>
+        /// 日志级别
+        /// </summary>
+        public string Level { get; set; }
+
+        /// <summary>
+        /// 跟踪号
+        /// </summary>
+        public string TraceId { get; set; }
+
+        /// <summary>
+        /// 操作时间
+        /// </summary>
+        public string OperationTime { get; set; }
+
+        /// <summary>
+        /// 执行时间
+        /// </summary>
+        public string Duration { get; set; }
+
+        /// <summary>
+        /// IP
+        /// </summary>
+        public string Ip { get; set; }
+
+        /// <summary>
+        /// 主机
+        /// </summary>
+        public string Host { get; set; }
+
+        /// <summary>
+        /// 线程号
+        /// </summary>
+        public string ThreadId { get; set; }
+
+        /// <summary>
+        /// 浏览器
+        /// </summary>
+        public string Browser { get; set; }
+
+        /// <summary>
+        /// 请I去地址
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// 业务编号
+        /// </summary>
+        public string BussinessId { get; set; }
+
+        /// <summary>
+        /// 租户
+        /// </summary>
+        public string Tenant { get; set; }
+
+        /// <summary>
+        /// 应用程序
+        /// </summary>
+        public string Application { get; set; }
+
+        /// <summary>
+        /// 模块
+        /// </summary>
+        public string Module { get; set; }
+
+        /// <summary>
+        /// 类名
+        /// </summary>
+        public string Class { get; set; }
+
+        /// <summary>
+        /// 方法
+        /// </summary>
+        public string Method { get; set; }
+
+        /// <summary>
+        /// 参数
+        /// </summary>
+        public StringBuilder Params { get; set; }
+
+        /// <summary>
+        /// 操作人编号
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// 操作人
+        /// </summary>
+        public string Operator { get; set; }
+        
+        /// <summary>
+        /// 操作人角色
+        /// </summary>
+        public string Role { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Caption { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public StringBuilder Content { get; set; }
+
+        /// <summary>
+        /// Sql语句
+        /// </summary>
+        public StringBuilder Sql { get; set; }
+
+        /// <summary>
+        /// Sql参数
+        /// </summary>
+        public StringBuilder SqlParams { get; set; }
+
+        /// <summary>
+        /// 错误码
+        /// </summary>
+        public string ErrorCode { get; set; }
+
+        /// <summary>
+        /// 异常消息
+        /// </summary>
+        public string Exception { get; set; }
+
+        /// <summary>
+        /// 堆栈跟踪
+        /// </summary>
+        public string StackTrace { get; set; }
+        #endregion
+
+        #region Constructor(构造函数)
+        /// <summary>
+        /// 初始化一个<see cref="LogContent"/>类型的实例
+        /// </summary>
+        public LogContent()
+        {
+            Params=new StringBuilder();
+            Content = new StringBuilder();
+            Sql=new StringBuilder();
+            SqlParams=new StringBuilder();
+        }
+        #endregion
+
+
+    }
+}
--- src/JCE.Utils/Logs/Extensions/LogExtensions.cs
+++ src/JCE.Utils/Logs/Extensions/LogExtensions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using JCE.Utils.Logs.Abstractions;
+using JCE.Utils.Logs.Core;
 
 namespace JCE.Utils.Logs.Extensions
 {
@@ -34,6 +35,109 @@ namespace JCE.Utils.Logs.Extensions
         public static ILog ContentLine(this ILog log, string value, params object[] args)
         {
             return log.Set<ILogContent>(content => content.ContentLine(value, args));
+        }
+
+        /// <summary>
+        /// 设置业务编号
+        /// </summary>
+        /// <param name="log">日志操作</param>
+        /// <param name="bussinessId">业务编号</param>
+        /// <returns></returns>
+        public static ILog BussinessId(this ILog log, string bussinessId)
+        {
+            return log.Set<LogContent>(content => content.BussinessId = bussinessId);
+        }
+
+        /// <summary>
+        /// 设置模块
+        /// </summary>
+        /// <param name="log">日志操作</param>
+        /// <param name="module">业务编号</param>
+        /// <returns></returns>
+        public static ILog Module(this ILog log, string module)
+        {
+            return log.Set<LogContent>(content => content.Module = module);
+        }
+
+        /// <summary>
+        /// 设置类名
+        /// </summary>
+        /// <param name="log">日志操作</param>
+        /// <param name="class">类名</param>
+        /// <returns></returns>
+        public static ILog Class(this ILog log, string @class)
+        {
+            return log.Set<LogContent>(content => content.Class = @class);
+        }
+
+        /// <summary>
+        /// 设置方法
+        /// </summary>
+        /// <param name="log">日志操作</param>
+        /// <param name="method">方法</param>
+        /// <returns></returns>
+        public static ILog Method(this ILog log, string method)
+        {
+            return log.Set<LogContent>(content => content.Method = method);
+        }
+
+        /// <summary>
+        /// 设置参数
+        /// </summary>
+        /// <param name="log">日志操作</param>
+        /// <param name="value">值</param>
+        /// <param name="args">变量值</param>
+        /// <returns></returns>
+        public static ILog Params(this ILog log, string value, params object[] args)
+        {
+            return log.Set<LogContent>(content => content.Append(content.Params, value, args));
+        }
+
+        /// <summary>
+        /// 设置参数并换行
+        /// </summary>
+        /// <param name="log">日志操作</param>
+        /// <param name="value">值</param>
+        /// <param name="args">变量值</param>
+        /// <returns></returns>
+        public static ILog ParamsLine(this ILog log, string value, params object[] args)
+        {
+            return log.Set<LogContent>(content => content.AppendLine(content.Params, value, args));
+        }
+
+        /// <summary>
+        /// 设置标题
+        /// </summary>
+        /// <param name="log">日志操作</param>
+        /// <param name="caption">标题</param>
+        /// <returns></returns>
+        public static ILog Caption(this ILog log, string caption)
+        {
+            return log.Set<LogContent>(content => content.Caption = caption);
+        }
+
+        /// <summary>
+        /// 设置Sql语句
+        /// </summary>
+        /// <param name="log">日志操作</param>
+        /// <param name="value">值</param>
+        /// <param name="args">变量值</param>
+        /// <returns></returns>
+        public static ILog Sql(this ILog log, string value, params object[] args)
+        {
+            return log.Set<LogContent>(content => content.Append(content.Sql, value, args));
+        }
+
+        /// <summary>
+        /// 设置Sql语句
+        /// </summary>
+        /// <param name="log">日志操作</param>
+        /// <param name="value">值</param>
+        /// <param name="args">变量值</param>
+        /// <returns></returns>
+        public static ILog SqlLine(this ILog log, string value, params object[] args)
+        {
+            return log.Set<LogContent>(content => content.AppendLine(content.Sql, value, args));
         }
     }
 }
