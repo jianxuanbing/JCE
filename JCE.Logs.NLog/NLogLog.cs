89bfdb70cad7564abf8c52e8269d7d6fcef9bad2
jian玄冰 <jianxuanhuo1@126.com>
2017-09-18 15:12:31 +0800

    提交日志

--- JCE.Logs.NLog/NLogLog.cs
+++ JCE.Logs.NLog/NLogLog.cs
@@ -3,10 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JCE.Utils.Logs.Abstractions;
+using JCE.Utils.Logs.Core;
+using NLog;
 
 namespace JCE.Logs.NLog
 {
-    public class NLogLog
+    /// <summary>
+    /// NLog日志
+    /// </summary>
+    public class NLogLog:LogBase<LogContent>
     {
+        /// <summary>
+        /// 类名
+        /// </summary>
+        private readonly string _class;
+
+        /// <summary>
+        /// 初始化一个<see cref="NLogLog"/>类型的实例
+        /// </summary>
+        /// <param name="provider">日志提供程序</param>
+        /// <param name="context">日志上下文</param>
+        /// <param name="class">类名</param>
+        internal NLogLog(ILogProvider provider, ILogContext context,string @class) : base(provider, context)
+        {
+            _class = @class;
+        }
+
+        /// <summary>
+        /// 获取日志内容
+        /// </summary>
+        /// <returns></returns>
+        protected override LogContent GetContent()
+        {
+            return new LogContent() {Class = _class};
+        }
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="content">日志内容</param>
+        protected override void Init(LogContent content)
+        {
+            base.Init(content);            
+        }
+
+        ///// <summary>
+        ///// 获取日志操作实例
+        ///// </summary>
+        ///// <returns></returns>
+        //public static ILog GetLog()
+        //{
+        //    return GetLog(string.Empty);
+        //}
+
+        ///// <summary>
+        ///// 获取日志操作实例
+        ///// </summary>
+        ///// <param name="instance">实例</param>
+        ///// <returns></returns>
+        //public static ILog GetLog(object instance)
+        //{
+        //    if (instance == null)
+        //    {
+        //        return GetLog();
+        //    }
+        //    var className = instance.GetType().ToString();
+        //    return GetLog(className, className);
+        //}
+
+        ///// <summary>
+        ///// 获取人日志操作实例
+        ///// </summary>
+        ///// <param name="logName">日志名称</param>
+        ///// <returns></returns>
+        //public static ILog GetLog(string logName)
+        //{
+        //    return GetLog(logName, string.Empty);
+        //}
+
+        //public static ILog GetLog(string logName, string @class)
+        //{
+        //    return GetLog();
+        //}
+
+        /// <summary>
+        /// 获取日志操作实例
+        /// </summary>
+        /// <param name="logger">日志操作</param>
+        /// <param name="format">日志格式化器</param>
+        /// <param name="context">日志上下文</param>
+        /// <param name="class">类名</param>
+        /// <returns></returns>
+        internal static ILog GetLog(Logger logger, ILogFormat format, ILogContext context, string @class)
+        {
+            return new NLogLog(new NLogLogProvider(logger,format), context,@class);
+        }
     }
 }
--- JCE.Logs.NLog/NLogLogManager.cs
+++ JCE.Logs.NLog/NLogLogManager.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JCE.Utils.Logs;
+using JCE.Utils.Logs.Abstractions;
+using JCE.Utils.Logs.Formats;
 
 namespace JCE.Logs.NLog
 {
-    public class NLogLogManager
+    /// <summary>
+    /// 日志管理服务
+    /// </summary>
+    public class NLogLogManager:ILogManager
     {
+        /// <summary>
+        /// 日志上下文
+        /// </summary>
+        public ILogContext Context { get; set; }
+
+        /// <summary>
+        /// 日志格式化器
+        /// </summary>
+        public ILogFormat Format { get; set; }
+
+        /// <summary>
+        /// 初始化一个<see cref="NLogLogManager"/>类型的实例
+        /// </summary>
+        /// <param name="context">日志伤心爱问</param>
+        public NLogLogManager(ILogContext context)
+        {
+            Context = context;
+            Format=new TextContentFormat();
+        }
+
+        public ILog GetLog()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILog GetLog(object instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILog GetLog(string logName)
+        {
+            throw new NotImplementedException();
+        }
+
+        private ILog GetLog(string logName, string @class)
+        {
+            throw new NotImplementedException();
+            //return NLogLog
+        }
     }
 }
