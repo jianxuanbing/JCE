ed97d0e9c847bf6a137875c157c0d49d3c16e3e7
jian玄冰 <jianxuanhuo1@126.com>
2017-11-20 13:53:58 +0800

    加入生成版本 以及测试

---
+++ JCE.Utils.Test/Modes/TimestampIdTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JCE.Utils.Modes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JCE.Utils.Test.Modes
+{
+    [TestClass]
+    public class TimestampIdTest
+    {
+        [TestMethod]
+        public void Test_GetId()
+        {
+            var result=TimestampId.GetInstance().GetId();
+            Console.WriteLine(result);
+        }
+    }
+}
